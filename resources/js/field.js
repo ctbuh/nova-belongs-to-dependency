cd8bce8684df6fd657c1be825cd269b60aceaa23
Jose Erick Carreon <support@joserick.com>
2023-02-15 01:18:37 -0600

    chore: Applied syntax es6

--- resources/js/field.js
+++ resources/js/field.js
@@ -1,7 +1,10 @@
+import IndexBelongsToDependency from './components/IndexBelongsToField'
+import DetailBelongsToDependency from './components/DetailBelongsToField'
+import FormBelongsToDependency from './components/FormField'
 
 Nova.booting((Vue) => {
     // Re-use old components but we extend the form field
-    Vue.component('IndexBelongsToDependency', require("./components/IndexBelongsToField.vue").default);
-    Vue.component('DetailBelongsToDependency', require("./components/DetailBelongsToField.vue").default);
-    Vue.component('FormBelongsToDependency', require("./components/FormField.vue").default);
+    Vue.component('IndexBelongsToDependency', IndexBelongsToDependency);
+    Vue.component('DetailBelongsToDependency', DetailBelongsToDependency);
+    Vue.component('FormBelongsToDependency', FormBelongsToDependency);
 });
