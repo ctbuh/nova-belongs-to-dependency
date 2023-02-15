7882b2f6a95eeb1d419bbea151e90e1ee048954c
Jose Erick Carreon <support@joserick.com>
2023-02-14 23:26:19 -0600

    fix: Load of components for views

--- resources/js/field.js
+++ resources/js/field.js
@@ -1,11 +1,7 @@
 
 Nova.booting((Vue) => {
     // Re-use old components but we extend the form field
-    Vue.component('IndexBelongsToDependency', Vue.component('IndexBelongsToField'));
-    Vue.component('DetailBelongsToDependency', Vue.component('DetailBelongsToField'));
-    Vue.component(
-        "FormBelongsToDependency",
-        require("./components/FormField.vue").default
-    );
+    Vue.component('IndexBelongsToDependency', require("./components/IndexBelongsToField.vue").default);
+    Vue.component('DetailBelongsToDependency', require("./components/DetailBelongsToField.vue").default);
+    Vue.component('FormBelongsToDependency', require("./components/FormField.vue").default);
 });
-
